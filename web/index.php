8b8c8264c6d324662654deae9e3a8710d5557dba
Dragos Plesca <plescadragoss@yahoo.com>
2012-10-30 08:17:15 +0200

    New start

--- web/index.php
+++ web/index.php
@@ -1,10 +1,23 @@
-<?php 
-require_once __DIR__.'/../vendor/autoload.php'; 
+<?php
+require_once __DIR__.'/../vendor/autoload.php';
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+require_once __DIR__.'/../vendor/config.php';
 
-$app = new Silex\Application(); 
-
-$app->get('/hello/{name}', function($name) use($app) { 
-    return 'Hello '.$app->escape($name); 
+$app->get('/', function () use ($app) {
+    return $app['twig']->render('index.twig', array());
 });
 
-$app->run(); 
+$app->post('/start', function (Request $request) use ($app) {
+	$track_params = array(
+		'method' => 'user.gettoptracks',
+		'user' => $request->get('username'),
+		'api_key' => $app['api_key'],
+		'limit' => '1',
+		'format' => 'json'
+	);
+	$track = file_get_contents( $app['endpoint'] . http_build_query($track_params, '', '&') ); 
+	echo '<pre>';print_r($track);die();
+});
+
+$app->run();
