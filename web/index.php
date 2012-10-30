877a84df65b0870048c64cd5fbd4ddbda0e197be
Plesca Dragos <plescadragoss@yahoo.com>
2012-10-30 18:43:08 +0200

    added all

--- web/index.php
+++ web/index.php
@@ -9,15 +9,19 @@ $app->get('/', function () use ($app) {
 });
 
 $app->post('/start', function (Request $request) use ($app) {
-	$track_params = array(
-		'method' => 'user.gettoptracks',
-		'user' => $request->get('username'),
-		'api_key' => $app['api_key'],
-		'limit' => '1',
-		'format' => 'json'
-	);
-	$track = file_get_contents( $app['endpoint'] . http_build_query($track_params, '', '&') ); 
-	echo '<pre>';print_r($track);die();
+	for($i = 0;$i <= 9;$i++){
+		$tracks = util::getSearchString($request->get('username'), $app['api_key'], $app['endpoint']);	
+		foreach ($tracks['tracks']['feed']['entry'] as $key => $value) {
+
+			$track_title = 	$tracks['tracks']['feed']['entry'][$key]['title']['$t'];
+			$guid = str_replace(':', '', strrchr($tracks['tracks']['feed']['entry'][$key]['id']['$t'], ':'));
+			$link = util::getMediaLink($guid);
+			if ($link !== false){
+				file_put_contents('log', 'Try: ' . $i . ', video: ' . $key . ' ' . $track_title . ', search string: ' . $tracks['search_string'] .PHP_EOL , FILE_APPEND);
+				echo json_encode( array('title' => $track_title, 'url' => $link) );exit;
+			}
+		}
+	}
 });
 
 $app->run();
